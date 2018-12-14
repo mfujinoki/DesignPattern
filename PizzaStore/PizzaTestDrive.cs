4120d03de5e10e6acc9f9908cd887346d481e6df
Mamoru Fujinoki <mfujinoki@fujibiz.com>
2018-12-13 17:17:22 -0800

    Create PizzaStore

---
+++ PizzaStore/PizzaTestDrive.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace PizzaStore
+{
+    public class PizzaTestDrive
+    {
+        public static void Main(string[] args)
+        {
+            PizzaStore nyStore = new NYStylePizzaStore();
+            PizzaStore chicagoStore = new ChicagoStylePizzaStore();
+
+            Pizza pizza = nyStore.orderPizza("cheese");
+            Console.WriteLine("Ethan ordered a " + pizza.name);
+
+            pizza = chicagoStore.orderPizza("cheese");
+            Console.WriteLine("Joel ordered a " + pizza.name);
+        }
+    }
+}
